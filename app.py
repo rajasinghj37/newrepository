417d3a869bd2f8bfc46884d5bfcc598ec306fb50
Rajasingh <rajasingh.j37@gmail.com>
2022-09-05 21:49:15 -0700

    Webpage created

--- app.py
+++ app.py
@@ -16,13 +16,19 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    newdata=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    print(np.array(list(data.values())))
+    newdata=scalar.transform(np.array(list(data.values())))
     output=linearmodel.predict(newdata)
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = linearmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text="The prediction of Sales is {}".format(output))
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
